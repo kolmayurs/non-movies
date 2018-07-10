0f5b12b50136819c4a3c044cf4a29b6e3bdd5416
Mayur Koli <kolimayurs@gmail.com>
2018-07-10 17:10:08 +0530

    commit

--- src/components/EventCard.js
+++ src/components/EventCard.js
@@ -4,6 +4,27 @@ import "../css/App.css";
 class EventCard extends Component {
 
   render() {
+    if(this.props.etsmallcard){
+      return(
+        <table cellPadding="0" cellSpacing="0" className="deviceWidth" border="0" align="center" style={{width:"570px", padding:"10px 10px 10px 10px",backgroundColor:"#ffffff", margin:"0 auto",}}>
+  <tr>
+    <td valign="top" style={{width:"100%", backgroundColor:"#ffffff",}}><a href={this.props.eturl+'?&utm_source=NMMumbaiEvent23October2017&utm_medium=email&utm_campaign='+this.props.etutmcampaign+''} target="_blank" style={{textDecoration:"none", color:"#fff",}}><img src={'https://in.bmscdn.com/events/mobile/'+this.props.etcode+'.jpg'} alt={this.props.etname}  title={this.props.etname}  border="0" style={{width:"100%", display:"block", backgroundColor:"#ffffff", color:"#000000", overflow:"hidden", borderRadius:"10px"}}/><img src="https://in.bmscdn.com/mailers/images/180207nm/banner_shadow.jpg" alt="" title="" border="0" style={{width:"100%", display:"block", backgroundColor:"#ffffff", color:"#000000"}}/></a></td>
+  </tr>
+  <tr>
+    <td className="deviceWidth" align="center" style={{ backgroundColor:"#ffffff,"}}>
+      <table className="deviceWidth" cellSpacing="0" cellPadding="0" align="center" style={{width:"570px", margin:"0px auto",borderSpacing:"0",}}>
+        <tr>
+          <td colspan="2" style={{padding:"0px 0px 0px 0px", fontSize:"20px", fontWeight:"bold", verticalAlign:"top", fontFamily:"Verdana, Arial,sans-serif", textAlign:"left", color:"#333333", lineHeight:"26px",}}><a href={this.props.eturl+'?&utm_source=NMMumbaiEvent23October2017&utm_medium=email&utm_campaign='+this.props.etutmcampaign+''} target="_blank" style={{textDecoration:"none", color:"#333333", fontFamily:"Verdana, Arial,sans-serif",}}>{this.props.etname}</a><span style={{display:"block",lineHeight:"18px", fontSize:"15px", padding:"5px 0 5px 0",fontWeight:"normal", color:"#666666",}}>{this.props.etvenues}</span><span style={{display:"block", padding:"5px 0 0px 0", lineHeight:"16px", fontSize:"12px",fontWeight:"normal", color:"#a3a3a3",}}>{this.props.etgenre}</span></td>
+        </tr>
+        <tr>
+          <td valign="middle" style={{width:"100%", padding:"10px 0px 0px 0px", fontSize:"20px", fontWeight:"bold", verticalAlign:"top", fontFamily:"Verdana, Arial,sans-serif", textAlign:"left", color:"#333333", lineHeight:"26px",}}><span style={{display:"block", padding:"5px 0px 15px 0px", fontSize:"15px", fontWeight:"bold", fontFamily:"Verdana, Arial,sans-serif", textAlign:"left", color:"#666666", lineHeight:"16px",}}>{this.props.etfirstdate}</span></td>
+          <td valign="top" style={{minWidth:'108px',padding:'0px 0px 10px 0px', textAlign:"left"}}><a href={this.props.eturl+'?&utm_source=NMMumbaiEvent23October2017&utm_medium=email&utm_campaign='+this.props.etutmcampaign+''} style={{textDecoration:"none", color:"#ffffff",}}><span style={{padding:"12px 30px 12px 30px", lineHeight:"40px", fontFamily:"Verdana, Arial,sans-serif", fontSize:"15px", whiteSpace:"nowrap", color:"#ffffff", backgroundColor:"#0072ff", fontWeight:"bold", borderRadius:"4px",}}>BOOK</span></a></td>
+        </tr>
+      </table>
+    </td>
+  </tr>
+</table>)
+    }
     return (
      <table className="deviceWidth" cellSpacing="0" cellPadding="0" align="left" style={{width:"40%", minWidth:"250px", maxWidth:"292px", margin:"10px 25px", backgroundColor:"#ffffff", verticalAlign:"top",display:"inline-block",float:"none",}}>
                                    <tbody>
@@ -12,7 +33,7 @@ class EventCard extends Component {
                                         <table cellPadding="0" cellSpacing="0" border="0" align="center" style={{backgroundColor:"#ffffff", margin:"0 auto"}}>
                                           <tbody>
                                           <tr>
-                                            <td valign="top" style={{width:"100%", backgroundColor:"#ffffff"}}><a href={this.props.eturl+'?&utm_source=NMMumbaiEvent23October2017&utm_medium=email&utm_campaign='+this.props.etutmcampaign+''} target="_blank" rel="noopener noreferrer" style={{textDecoration:"none", color:"#fff"}}><img src={'https://in.bmscdn.com/events/eventlisting/'+this.props.etcode+'.jpg'} alt={this.props.etname} title={this.props.etname} border="0" style={{width:"100%", display:"block", backgroundColor:"#ffffff", color:"#010101", overflow:"hidden", borderRadius:"10px"}}/></a></td>
+                                            <td valign="top" style={{width:"100%", backgroundColor:"#ffffff"}}><a href={this.props.eturl+'?&utm_source=NMMumbaiEvent23October2017&utm_medium=email&utm_campaign='+this.props.etutmcampaign+''} target="_blank" rel="noopener noreferrer" style={{textDecoration:"none", color:"#fff"}}><img src={'https://in.bmscdn.com/events/mobile/'+this.props.etcode+'.jpg'} alt={this.props.etname} title={this.props.etname} border="0" style={{width:"100%", display:"block", backgroundColor:"#ffffff", color:"#010101", overflow:"hidden", borderRadius:"10px"}}/></a></td>
                                           </tr>
                                           <tr>
                                             <td style={{width:"100%",padding:"10px 5px 0px 5px", fontSize:"16px", verticalAlign:"top", fontFamily:"Verdana, Arial,sans-serif", textAlign:"left", color:"#333333", lineHeight:"22px"}}><a href={this.props.eturl+'?&utm_source=NMMumbaiEvent23October2017&utm_medium=email&utm_campaign='+this.props.etutmcampaign+''} target="_blank" rel="noopener noreferrer" style={{textDecoration:"none", color:"#333333", fontFamily:"Verdana, Arial,sans-serif"}}>{this.props.etname}</a></td>
