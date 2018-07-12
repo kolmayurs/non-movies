6aceed5c8b8f112a5b68cdcfffe7da04c9a6a55d
Mayur Koli <kolimayurs@gmail.com>
2018-07-12 18:09:54 +0530

    commit

---
+++ src/components/temp3.js
@@ -0,0 +1,18 @@
+<table cellPadding="0" cellSpacing="0" className="deviceWidth" border="0" align="center" style={{width:570px, padding:10px 10px 10px 10px,backgroundColor:#ffffff, margin:0 auto,}}>
+	<tr>
+		<td valign="top" style={{width:100%, backgroundColor:#ffffff,}}><a href={this.props.eturl+'?&utm_source=NMMumbaiEvent23October2017&utm_medium=email&utm_campaign='+this.props.etutmcampaign+''} target="_blank" style={{text-decoration:none, color:#fff,}}><img src="https://in.bmscdn.com/events/mobile/'+this.props.etcode+'.jpg" alt="'+this.props.etname+'" title="'+this.props.etname+'" border="0" style={{width:100%, display:block, backgroundColor:#ffffff, color:#000000, overflow:hidden, border-radius:10px,"/><img src="https://in.bmscdn.com/mailers/images/180207nm/banner_shadow.jpg" alt="" title="" border="0" style={{width:100%, display:block, backgroundColor:#ffffff, color:#000000,"/></a></td>
+	</tr>
+	<tr>
+		<td className="deviceWidth" align="center" style={{ backgroundColor:#ffffff,}}>
+			<table className="deviceWidth" cellSpacing="0" cellPadding="0" align="center" style={{width:570px, margin:0px auto,borderSpacing:0,}}>
+				<tr>
+					<td colspan="2" style={{padding:0px 0px 0px 0px, font-size:20px, fontWeight:bold, verticalAlign:top, fontFamily:Verdana, Arial,sans-serif, textAlign:left, color:#333333, lineHeight:26px,}}><a href={this.props.eturl+'?&utm_source=NMMumbaiEvent23October2017&utm_medium=email&utm_campaign='+this.props.etutmcampaign+''} target="_blank" style={{text-decoration:none, color:#333333, fontFamily:Verdana, Arial,sans-serif,}}>'+this.props.etname+'</a><span style={{display:block,lineHeight:18px, font-size:15px, padding:5px 0 5px 0,fontWeight:normal, color:#666666,}}>'+this.props.etvenues+'</span><span style={{display:block, padding:5px 0 0px 0, lineHeight:16px, font-size:12px,fontWeight:normal, color:#a3a3a3,}}>'+genreFormat(this.props.etgenre)+'</span></td>
+				</tr>
+				<tr>
+					<td valign="middle" style={{width:100%, padding:10px 0px 0px 0px, font-size:20px, fontWeight:bold, verticalAlign:top, fontFamily:Verdana, Arial,sans-serif, textAlign:left, color:#333333, lineHeight:26px,}}><span style={{display:block, padding:5px 0px 15px 0px, font-size:15px, fontWeight:bold, fontFamily:Verdana, Arial,sans-serif, textAlign:left, color:#666666, lineHeight:16px,}}>'+dateFormat(this.props.etfirstdate, this.props.etlastdate)+'</span></td>
+					<td valign="top" style={{min-width:108px,padding:0px 0px 10px 0px, textAlign:left}}><a href={this.props.eturl+'?&utm_source=NMMumbaiEvent23October2017&utm_medium=email&utm_campaign='+this.props.etutmcampaign+''} style={{text-decoration:none, color:#ffffff,}}><span style={{padding:12px 30px 12px 30px, lineHeight:40px, fontFamily:Verdana, Arial,sans-serif, font-size:15px, white-space:nowrap, color:#ffffff, backgroundColor:#0072ff, fontWeight:bold, border-radius:4px,}}>BOOK</span></a></td>
+				</tr>
+			</table>
+		</td>
+	</tr>
+</table>
